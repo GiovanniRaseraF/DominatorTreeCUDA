e07d7bb7e2688fb482ac4ba14ad31604d8fc178c
GiovanniRaseraF <giovanni.raserafarra@gmail.com>
2023-02-24 17:56:29 +0100

    naive: testing execution

---
+++ sequential/naive/naive.cpp
@@ -0,0 +1,22 @@
+#include <algorithm>
+#include <iomanip>
+#include <iostream>
+#include <vector>
+//#include <execution>
+/*
+template< class ExecutionPolicy, class ForwardIt, class UnaryPredicate >
+bool none_of( ExecutionPolicy&& policy, ForwardIt first, ForwardIt last,
+              UnaryPredicate p );
+*/
+int main(){
+    std::cout << "TODO: naive solution" << std::endl;
+
+    std::vector<bool> vec(1000000, false);
+    std::cout << "vec.size(): " << vec.size() << std::endl;
+
+    auto ret = std::none_of(vec.begin(), vec.end(), [&](bool val){ return val == true;});
+    std::cout << "result: " << std::boolalpha << ret << std::endl;
+
+
+    return 0;
+}
