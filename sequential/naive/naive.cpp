49d3cbd0b98fd0b3949cbc8751403c301bcbe970
GiovanniRaseraF <giovanni.raserafarra@gmail.com>
2023-02-24 21:45:22 +0100

    naive: testing compiler gcc 11 on parallel execution policy

--- sequential/naive/naive.cpp
+++ sequential/naive/naive.cpp
@@ -1,22 +1,28 @@
+#include <parallel/algorithm>
+#include <thread>
 #include <algorithm>
-#include <iomanip>
+#include <numeric>
+#include <chrono>
+#include <functional>
+#include <execution>
 #include <iostream>
-#include <vector>
+#include <random>
+
 //#include <execution>
 /*
 template< class ExecutionPolicy, class ForwardIt, class UnaryPredicate >
 bool none_of( ExecutionPolicy&& policy, ForwardIt first, ForwardIt last,
               UnaryPredicate p );
 */
+
 int main(){
     std::cout << "TODO: naive solution" << std::endl;
 
     std::vector<bool> vec(1000000, false);
     std::cout << "vec.size(): " << vec.size() << std::endl;
 
-    auto ret = std::none_of(vec.begin(), vec.end(), [&](bool val){ return val == true;});
+    auto ret = std::none_of(std::execution::par_unseq, vec.begin(), vec.end(), [&](bool val){ return val == true;});
     std::cout << "result: " << std::boolalpha << ret << std::endl;
-
 
     return 0;
 }
