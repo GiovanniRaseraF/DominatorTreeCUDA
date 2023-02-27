28f4bc4a00fb3c3fcf57f3b7a70e4c81daa71cb9
GiovanniRaseraF <giovanni.raserafarra@gmail.com>
2023-02-27 16:05:36 +0100

    compile: fix openmp compilation for cmake

--- testing/compile/compile.cu
+++ testing/compile/compile.cu
@@ -8,10 +8,17 @@ int main(){
     std::cout << "cuda compile" << std::endl;
 
     int *a;
-    cudaMalloc(&a, 10000000);
+    cudaMalloc(&a, 10);
     cudaFree(a);
 
-    std::cout << "thread num: " << omp_get_thread_num() << std::endl;
+    #pragma omp parallel
+    {
+        #pragma omp critical
+        {
+            std::cout << "thread num: " << omp_get_thread_num() << std::endl;
+        }
+    }
+
     std::cout << "result: " << "passed" << std::endl;
 
     return 0;
