3f66c870d10251478597eb7dcb5534fe58fea668
GiovanniRaseraF <giovanni.raserafarra@gmail.com>
2023-03-24 14:42:06 +0100

    omp: omp for schedule types

--- testing/openmp/omp.cu
+++ testing/openmp/omp.cu
@@ -8,9 +8,12 @@
 
 int main(){
     std::cout << "omp test cudaversion: "  << std::endl;
+
     std::vector<int> vet(99999999, 1); 
+
     int partialsum = 0;
     int totalsum = 0;
+
     const int maxthreads = omp_get_max_threads();
     const int range = std::floor(vet.size() / maxthreads);
     
@@ -35,8 +38,37 @@ int main(){
             totalsum += partialsum;
         }
     }
-
     std::cout << "result: " << totalsum << std::endl;
+
+    std::cout << "OMP for test" << std::endl;
+    // parallel for
+    long sum = 0;
+    std::vector<long> values(1 << 20, 1);
+    unsigned long npoints = values.size();
+
+    #pragma omp parallel shared(npoints) reduction(+: sum) num_threads(8)
+    {
+        sum = 0;
+
+        #pragma omp for
+        for(unsigned long i = 0; i < npoints; i++){
+            sum+=values[i];
+        }
+    }
+    std::cout << "ompsum: " << sum << std::endl;
+
+    sum = 0;
+    // schedule
+    #pragma omp parallel shared(npoints) reduction(+: sum) num_threads(8)
+    {
+        sum = 0;
+
+        #pragma omp for schedule(dynamic, 1000)
+        for(int i = 0; i < npoints; i++){
+            sum+=values[i];
+        }
+    }
+    std::cout << "sum schedule(dynamic): " << sum << std::endl;
 
     return 0;
 }
