693766760be5dd327bfda55cc4709d31df14e950
cetriolino <45256169+GiovanniRaseraF@users.noreply.github.com>
2024-09-18 11:53:30 +0200

    test: correct parallel reduce

--- testing/examples_lessons/reductionprofiler/parallelreduction.cu
+++ testing/examples_lessons/reductionprofiler/parallelreduction.cu
@@ -13,9 +13,10 @@
 
 #define N threads * blocks
 
+#if defined(NAIVE_REDUCE)
 int factor = 1;
-__global__ void reduceGPU(float *A, float *Results){
-    __shared__ float shared_mem[threads];
+__global__ void reduceGPU(int *A, int *Results){
+    __shared__ int shared_mem[threads];
 
     // indexing
     unsigned int tid = threadIdx.x;
@@ -36,19 +37,133 @@ __global__ void reduceGPU(float *A, float *Results){
     __syncthreads();
 }
 
-float reduce(float *h_Results, float *A, float *Results);
-float reduce(float *h_Results, float *A, float *Results){
+#elif defined(FIRST_ATTEMPT_REDUCE)
+int factor = 1;
+extern "C"{
+    std::string reduce_type{"FIRST_ATTEMPT"};
+}
+
+__global__ void reduceGPU(int *A, int *Results){
+    __shared__ int shared_mem[threads];
+
+    // indexing
+    unsigned int tid = threadIdx.x;
+    unsigned int index_in_global = (blockIdx.x * blockDim.x) + tid;
+
+    shared_mem[tid] = A[index_in_global];
+    __syncthreads();
+
+    for(unsigned int s = 1; s < blockDim.x; s *= 2){
+        if(tid % (s * 2) == 0){
+            shared_mem[tid] += shared_mem[tid + s];
+        }
+
+        __syncthreads();
+    }
+
+    if(tid == 0) Results[blockIdx.x] = shared_mem[0];
+}
+
+#elif defined(SECOND_REDUCE)
+int factor = 1;
+extern "C"{
+    std::string reduce_type{"SECOND_REDUCE"};
+}
+
+__global__ void reduceGPU(int *A, int *Results){
+    __shared__ int shared_mem[threads];
+
+    // indexing
+    unsigned int tid = threadIdx.x;
+    unsigned int index_in_global = (blockIdx.x * blockDim.x) + tid;
+
+    shared_mem[tid] = A[index_in_global];
+    __syncthreads();
+
+    for(unsigned int s = 1; s < blockDim.x; s *= 2){
+        int index = 2 * s * tid;
+
+        if(index < blockDim.x){
+            shared_mem[tid] += shared_mem[index + s];
+        }
+
+        __syncthreads();
+    }
+
+    if(tid == 0) Results[blockIdx.x] = shared_mem[0];
+}
+#elif defined(THIRD_REDUCE)
+int factor = 1;
+extern "C"{
+    std::string reduce_type{"THIRD_REDUCE"};
+}
+
+__global__ void reduceGPU(int *A, int *Results){
+    __shared__ int shared_mem[threads];
+
+    // indexing
+    unsigned int tid = threadIdx.x;
+    unsigned int index_in_global = (blockIdx.x * blockDim.x) + tid;
+
+    shared_mem[tid] = A[index_in_global];
+    __syncthreads();
+
+    for(unsigned int s = blockDim.x / 2; s > 0; s >>= 1){
+        if(tid < s){
+            shared_mem[tid] += shared_mem[tid + s];
+        }
+
+        __syncthreads();
+    }
+
+    if(tid == 0) Results[blockIdx.x] = shared_mem[0];
+}
+#elif defined(SPEED_REDUCE)
+int factor = 2;
+
+__global__ void reduceGPU(int *A, int *Results){
+    __shared__ int shared_mem[threads];
+
+    // indexing
+    unsigned int tid = threadIdx.x;
+    unsigned int index_in_global = (blockIdx.x * (2 * blockDim.x)) + tid;
+
+    shared_mem[tid] = A[index_in_global] + A[index_in_global + blockDim.x];
+    __syncthreads();
+
+    for(unsigned int s = blockDim.x / 2; s > 0; s >>= 1){
+        if(tid < s){
+            shared_mem[tid] += shared_mem[tid + s];
+        }
+
+        __syncthreads();
+    }
+
+    if(tid == 0) Results[blockIdx.x] = shared_mem[0];
+}
+#else
+extern "C"{
+    std::string reduce_type{"NOT_IMPLEMENTED_REDUCE"};
+}
+__global__ void reduceGPU(int *A, int *Results){
+    print("Error on call");
+    exit(-1);
+}
+#endif
+
+int reduce(int *h_Results, int *A, int *Results);
+int reduce(int *h_Results, int *A, int *Results){
     reduceGPU<<<dim3(blocks), dim3(threads / factor)>>>(A, Results);
 
     // retreve result   
     cudaMemcpy(
         h_Results,
         Results,
-        N*sizeof(float),
+        N*sizeof(int),
         cudaMemcpyDeviceToHost
     );
 
-    std::vector<float> res{};
+    std::vector<int> res{};
     res.assign(h_Results, h_Results+N);
     auto sum = std::accumulate(res.begin(), res.end(), 0);
 
@@ -57,26 +172,27 @@ float reduce(float *h_Results, float *A, float *Results){
 
 int main(){
     //on host
-    float h_A[N], h_Results[N];
-    float *A, *Results;
+    int h_A[N], h_Results[N];
+    int *A, *Results;
+    srand(time(NULL));
 
     for(int i = 0; i < N; i++){
-        h_A[i] = rand();
+        h_A[i] = (rand() % 100) + 10;
         h_Results[i] = 0;
     }
 
     cudaMalloc(
         (void**)&A,
-        N*sizeof(float));
+        N*sizeof(int));
     cudaMalloc(
         (void**)&Results,
-        N*sizeof(float));
+        N*sizeof(int));
 
     // copy
     cudaMemcpy(
         A,
         h_A,
-        N*sizeof(float),
+        N*sizeof(int),
         cudaMemcpyHostToDevice
     );
     
@@ -85,11 +201,14 @@ int main(){
     cudaMemcpy(
         Results,
         h_Results,
-        N*sizeof(float),
+        N*sizeof(int),
         cudaMemcpyHostToDevice
     );
 
-    std::cout << "result= " << sum << ", correct?" << std::boolalpha << " " << (sum == N) << std::endl;
+    std::vector<int> res{};
+    res.assign(h_A, h_A+N);
+    auto actual_sum = std::accumulate(res.begin(), res.end(), 0);
+    std::cout << std::endl << ": result= " << sum << " | expected= " << actual_sum << ", correct?" << std::boolalpha << " " << (sum == actual_sum) << std::endl;
 
     cudaFree(A);
     cudaFree(Results);
