dcb43c08b4ab4ae842121922622b3184d22badb6
cetriolino <45256169+GiovanniRaseraF@users.noreply.github.com>
2024-09-26 15:26:55 +0200

    dijkstra: update name

---
+++ sequential/dijkstra/v3.cpp
@@ -0,0 +1,5 @@
+// Scrivere un programma che, dato un nodo x in V (diverso da s), determini un sottoinsieme D di nodi tale che:
+// 1) la sorgente s non appartiene a D,   il nodo x non appartiene a D
+// 2) ogni cammino diretto da s a x passa per almeno un nodo di D
+// 3) D è minimale rispetto alla cardinalità (ovvero non esiste un altro
+// insieme che soddisfi le proprietà 1 e 2 e che abbia meno elementi di D)
