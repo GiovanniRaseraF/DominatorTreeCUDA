96c65bcc5b6db0ad748e71a901faead11dfafdf6
Giovanni Rasera <giovanni.raserafarra@gmail.com>
2024-09-26 08:21:56 +0200

    v2: update version and indentation

--- sequential/dikstra/v2.cpp
+++ sequential/dikstra/v2.cpp
@@ -8,51 +8,51 @@
 #include <thread>
 #include <future>
 #include <memory>
+#include <iomanip>
 
 using namespace std::chrono_literals;
 using namespace std::chrono;
  
 // Number of vertices in the graph
-constexpr int V = 512;
-constexpr int THREADSCOUNT = 8;
-constexpr int step = V / THREADSCOUNT;
+constexpr int NODES = 512;
+constexpr int THREADSCOUNT = 16;
+constexpr int step = NODES / THREADSCOUNT;
  
-int minDistance(std::array<int, V> &dist, bool sptSet[])
+int minDistance(std::array<int, NODES> &dist, bool sptSet[])
 {
     // Initialize min value
     int min = INT_MAX, min_index;
  
-    for (int v = 0; v < V; v++)
+    for (int v = 0; v < NODES; v++)
         if (sptSet[v] == false && dist[v] <= min)
             min = dist[v], min_index = v;
  
     return min_index;
 }
  
-void printSolution(std::array<int, V> &dist, int n)
-{
-    printf("Vertex   Distance from Source\n");
-    for (int i = 0; i < V; i+=1)
+void printSolution(std::array<int, NODES> &dist, int n){
+    std::cout << ("Vertex   Distance from Source") << std::endl;
+
+    for (int i = 0; i < NODES; i+=1)
         printf("\t%d \t\t\t\t %s\n", i, (dist[i] == INT_MAX ? "CANNOT REACH" : "REACHABLE"));
 }
  
-std::array<int, V> dijkstra(std::array<std::array<int, V>, V> &graph, int src)
-{
-    std::array<int, V> dist; 
-    bool sptSet[V]; 
+std::array<int, NODES> dijkstra(std::array<std::array<int, NODES>, NODES> &graph, int src){
+    std::array<int, NODES> dist; 
+    bool sptSet[NODES]; 
                     
-    for (int i = 0; i < V; i++)
+    for (int i = 0; i < NODES; i++)
         dist[i] = INT_MAX, sptSet[i] = false;
  
     dist[src] = 0;
  
-    for (int count = 0; count < V - 1; count++) {
+    for (int count = 0; count < NODES - 1; count++) {
         int u = minDistance(dist, sptSet);
  
         sptSet[u] = true;
  
         // picked vertex.
-        for (int v = 0; v < V; v++)
+        for (int v = 0; v < NODES; v++)
             if (
                 !sptSet[v] && graph[u][v] 
                 && dist[u] != INT_MAX 
@@ -67,20 +67,21 @@ std::array<int, V> dijkstra(std::array<std::array<int, V>, V> &graph, int src)
 //
 // Can you reach destination from source ? 
 //
-bool isDestinationReachable(std::array<std::array<int, V>, V> &graph, int source, int destination){
+bool isDestinationReachable(std::array<std::array<int, NODES>, NODES> &graph, int source, int destination){
     auto solution = dijkstra(graph, source);
-    //printSolution(solution, V);
+    //printSolution(solution, NODES);
     if (solution[destination] == INT_MAX) return false;
     else return true;
 }
 
-void perThreadExecute(std::array<std::array<int, V>, V> graph, int source, int destination, int threadIdx){
-    auto canReach = true;//isDestinationReachable(graph, source, destination);
+void perThreadExecute(std::array<std::array<int, NODES>, NODES> graph, int source, int destination, int threadIdx){
+    auto canReach = true;
+    //isDestinationReachable(graph, source, destination);
     // need to chech if is reachable
     if(canReach){
         {
             for(int i = (threadIdx-1) * step; i < ((threadIdx-1) * step) + step; i++)
-                for(int j = 0; j < V; j++){
+                for(int j = 0; j < NODES; j++){
                     {
                         // modifier
                         int store = graph[i][j];
@@ -88,7 +89,7 @@ void perThreadExecute(std::array<std::array<int, V>, V> graph, int source, int d
                 
                         auto canReach = isDestinationReachable(graph, source, destination);
                         if(canReach == false){
-                            //std::cout << "arch " << i << ", " << j << "\n";
+                            // here i can add the shared store
                         }
                         graph[i][j] = store;
                     }
@@ -101,9 +102,9 @@ void perThreadExecute(std::array<std::array<int, V>, V> graph, int source, int d
 int main()
 {
     srand(0);
-    std::array<std::array<int, V>, V> graph0;
+    std::array<std::array<int, NODES>, NODES> graph0;
 
-    //int graph[V][V] = 
+    //int graph[NODES][NODES] = 
     //{ //  0  1  2  3  4  5  6  7  8 
         //{ 0, 0, 0, 0, 0, 0, 0, 1, 0 }, // 0
         //{ 0, 0, 0, 0, 0, 0, 0, 0, 0 }, // 1
@@ -118,8 +119,8 @@ int main()
     std::array<std::future<void>, THREADSCOUNT> futures;
 
     // populate graph
-    for(int i = 0; i < V; i++){
-        for(int j = 0; j < V; j++){
+    for(int i = 0; i < NODES; i++){
+        for(int j = 0; j < NODES; j++){
             int temp = rand() % 1;
             graph0[i][j] = temp;
             //graph0[i][j] = graph[i][j];
@@ -128,9 +129,9 @@ int main()
 
 
     // now calculate
-    std::cout << "Threads: " << THREADSCOUNT << std::endl;
-    std::cout << "V: " << V << std::endl;
-    std::cout << "V*V: " << V*V << std::endl;
+    std::cout << std::setw(20) << "Threads: " << THREADSCOUNT << std::endl;
+    std::cout << std::setw(20) << "NODES: " << NODES << std::endl;
+    std::cout << std::setw(20) << "NODES*NODES: " << NODES*NODES << std::endl;
     std::cout << "Sub if you remove: >>>" << std::endl;
 
     std::chrono::time_point<std::chrono::steady_clock> start = std::chrono::steady_clock::now();
@@ -144,7 +145,7 @@ int main()
                     std::launch::async, 
                     [&](int index){
                         // pass every thing
-                        perThreadExecute(graph0, 0, V-1, index);
+                        perThreadExecute(graph0, 0, NODES-1, index);
                     },
                     threadIdx
                 );
