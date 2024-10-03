79607ae918a6aaef52bd655e83a69468382f9ef4
Giovanni Rasera <giovanni.raserafarra@gmail.com>
2024-10-03 11:23:04 +0200

    mincut: test on cpu working

--- minCut/main_tutorialspoint.cpp
+++ minCut/main_tutorialspoint.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <limits.h>
 #include <string.h>
+#include <array>
 #include <queue>
 using namespace std;
  
@@ -10,7 +11,7 @@ using namespace std;
  
 /* Returns true if there is a path from source 's' to sink 't' in
   residual graph. Also fills parent[] to store the path */
-int bfs(int rGraph[V][V], int s, int t, int parent[])
+int bfs(std::array<std::array<int, V>, V> &rGraph, int s, int t, int parent[])
 {
     // Create a visited array and mark all vertices as not visited
     bool visited[V];
@@ -48,7 +49,7 @@ int bfs(int rGraph[V][V], int s, int t, int parent[])
 // A DFS based function to find all reachable vertices from s.  The function
 // marks visited[i] as true if i is reachable from s.  The initial values in
 // visited[] must be false. We can also use BFS to find reachable vertices
-void dfs(int rGraph[V][V], int s, bool visited[])
+void dfs(std::array<std::array<int, V>, V> &rGraph, int s, bool visited[])
 {
     visited[s] = true;
     for (int i = 0; i < V; i++)
@@ -57,14 +58,14 @@ void dfs(int rGraph[V][V], int s, bool visited[])
 }
  
 // Prints the minimum s-t cut
-void minCut(int graph[V][V], int s, int t)
+void minCut(std::array<std::array<int, V>, V> &graph, int s, int t, std::array<std::array<int, V>, V> &rGraph)
 {
     int u, v;
  
     // Create a residual graph and fill the residual graph with
     // given capacities in the original graph as residual capacities
     // in residual graph
-    int rGraph[V][V]; // rGraph[i][j] indicates residual capacity of edge i-j
+    // rGraph[i][j] indicates residual capacity of edge i-j
     for (u = 0; u < V; u++)
         for (v = 0; v < V; v++)
              rGraph[u][v] = graph[u][v];
@@ -94,37 +95,66 @@ void minCut(int graph[V][V], int s, int t)
         }
     }
  
+    
+    
+ 
+    return;
+}
+
+void printResidual( std::array<std::array<int, V>, V> &graph, int s, int t, std::array<std::array<int, V>, V> &rGraph ){
+    minCut(graph, s, t, rGraph);
+    
     // Flow is maximum now, find vertices reachable from s
     bool visited[V];
     memset(visited, false, sizeof(visited));
     dfs(rGraph, s, visited);
- 
+
     // Print all edges that are from a reachable vertex to
     // non-reachable vertex in the original graph
     for (int i = 0; i < V; i++)
       for (int j = 0; j < V; j++)
          if (visited[i] && !visited[j] && graph[i][j])
               cout << i << " - " << j << endl;
- 
-    return;
 }
  
 // Driver program to test above functions
 int main()
 {
     // Let us create a graph shown in the above example
-    int graph[V][V] = { {0, 16, 13, 0, 0, 0 , 0},
-                        {0, 0, 10, 12, 0, 0, 0},
-                        {0, 4, 0, 0, 14, 0, 0},
-                        {0, 0, 9, 0, 0, 20, 0},
-                        {0, 0, 0, 7, 0, 4, 0},
-                        {0, 0, 0, 0, 0, 0, 0},
-                        {0, 0, 0, 0, 0, 0, 0}
-                      };
+    //int graph[V][V] = { {0, 16, 13, 0, 0, 0 , 0},
+                    //     {0, 0, 10, 12, 0, 0, 0},
+                    //     {0, 4, 0, 0, 14, 0, 0},
+                    //     {0, 0, 9, 0, 0, 20, 0},
+                    //     {0, 0, 0, 7, 0, 4, 0},
+                    //     {0, 0, 0, 0, 0, 0, 0},
+                    //     {0, 0, 0, 0, 0, 0, 0}
+                    //   };
  
-    minCut(graph, 0, 6);
+    //minCut(graph, 0, 6);
 
+    std::array<std::array<int, V>, V> graph = {{ 
+    //   0  1  2  3  4  5  6
+        {0, 1, 0, 1, 1, 1, 0}, // 0
+        {0, 0, 1, 0, 0, 0, 0}, // 1
+        {0, 0, 0, 0, 0, 0, 1}, // 2
+        {0, 0, 1, 0, 0, 0, 0}, // 3
+        {0, 0, 1, 0, 0, 0, 10}, // 4
+        {0, 0, 1, 0, 0, 0, 0}, // 5
+        {0, 0, 0, 0, 0, 0, 0}, // 6
+    }};
 
+    std::array<std::array<int, V>, V> rgraph = {{ 
+    //   0  1  2  3  4  5  6
+        {0, 1, 0, 1, 1, 1, 0}, // 0
+        {0, 0, 1, 0, 0, 0, 0}, // 1
+        {0, 0, 0, 0, 0, 0, 1}, // 2
+        {0, 0, 1, 0, 0, 0, 0}, // 3
+        {0, 0, 1, 0, 0, 0, 10}, // 4
+        {0, 0, 1, 0, 0, 0, 0}, // 5
+        {0, 0, 0, 0, 0, 0, 0}, // 6
+    }};
+
+    printResidual(graph, 0, 6, rgraph);
  
     return 0;
 }
