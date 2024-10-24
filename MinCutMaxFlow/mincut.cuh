a3c4eb40bcbe9e3eb1db2591f5ef4025041d1c0f
Giovanni Rasera <giovanni.raserafarra@gmail.com>
2024-10-24 15:48:54 +0200

    automatic: data exchange

--- MinCutMaxFlow/mincut.cuh
+++ MinCutMaxFlow/mincut.cuh
@@ -41,49 +41,28 @@ namespace parallel {
             - is relabled: if for all c(x, *) > 0, h(*) >= h(x)
     */
     namespace GoldbergTarjan{
-        // If the vertex is active, the thread
-        // u will find its neighbor vertex v whose height is minimum among other neighbor 
-        // vertices (line:10 - 13). The thread then push flow from u to v (line:15 - 19)
-        // when h(u) > h(v); otherwise, the thread will relabel the active vertex u.
-        __global__ void push(GPUGraph G, GPUGraph Gf, int V, GPUExcessFlow e, GPUHeight h, int HEIGHT_MAX){
-            // calcualte x with thread id instead of passing int
-            int x = threadIdx.x;
-            int u = x;
-            //printf("called push: %d, e[]:%d, height[]:%d, H_MAX:%d\n", x, e[x], height[x], HEIGHT_MAX);
 
-            if(false && x == 0){
-                for(int i = 0; i < V; i ++){
-                    for(int j = 0; j < V; j++){
-                        printf("%d ", Gf[i*V+j]);
-                    }
-                    printf("\n");
-                }
-            }
+        __global__ void pushrelable(GPUGraph G, GPUGraph Gf, int V, GPUExcessFlow e, GPUHeight h, int HEIGHT_MAX){
+            // calcualte x with thread id instead of passing int
+            int u = threadIdx.x;;
 
             if(e[u] > 0 && h[x] < HEIGHT_MAX){
                 // line 10 from 2404.00270v1.pdf
                 int hprime = INT_MAX;
                 for(int v = 0; v < V; v++){
                     if(Gf[u*V+v] > 0){ // is (u,v) £ Ef ?
-                        //printf("pushing: (%d, %d) £ Ef\n", u, v);
-                        // find min
                         hprime = min(hprime, h[v]);
                     }
                 }
-                //printf("h[%d]: %d, hprime: %d\n", u, h[u], hprime);
-                // line 14 from 2404.00270v1.pdf
+
                 if(h[u] > hprime){
                     for(int vprime = 0; vprime < V; vprime++){
-                        if(Gf[u*V+vprime] > 0 && h[u] > h[vprime]){ 
+                        if(Gf[u*V+vprime] > 0 && h[u] > h[vprime]+1){ 
                             int d = min(e[u], Gf[u*V+vprime]);
                             Gf[u*V+vprime]-=d;
                             e[u]-=d;
                             Gf[vprime*V+u]+=d;
                             e[vprime]-=d;
-
-                            // printf("u, v': (%d, %d) \n", u, vprime);
-                            // printf("d: %d\n", d);
-                            // atomic operations 
                         }
                     }
                 }else{
@@ -93,29 +72,7 @@ namespace parallel {
             }
         }
 
-        // __global__ void relable(GPUGraph G, GPUGraph Gf, int V, int x_unused, GPUExcessFlow e, GPUHeight height, int HEIGHT_MAX){
-        //     int x = threadIdx.x;
-        //     //printf("called relable: %d, e[]:%d, height[]:%d, H_MAX:%d\n", x, e[x], height[x], HEIGHT_MAX);
-        //     if(x == 0){
-        //         printf("relable print\n");
-        //         for(int i = 0; i < V; i ++){
-        //             for(int j = 0; j < V; j++){
-        //                 printf("%d ", Gf[i*V+j]);
-        //             }
-        //             printf("\n");
-        //         }
-        //     }
-        //     if(e[x] > 0 && height[x] < HEIGHT_MAX){
-        //         int my_height = HEIGHT_MAX;
-        //         for(int y = 0; y < V; y++){
-        //             if(G[x*V+y] > 0){
-        //                 my_height = min(my_height, height[y]+1);
-        //             }
-        //         }
-        //         height[x] = my_height;
-        //     }
-        // }       
-
+        // Initialize the flow
         void preflow(const Graph &G, Graph &Gf, ExcessFlow &e, Excess_total &excessTotal){
             std::cout << "called Preflow" << std::endl;
             // maybe i can parallelize this
@@ -189,7 +146,7 @@ namespace parallel {
                 // Step 1: Push-relabel kernel (GPU)
                 int cicle = G.size(); // = |V|
                 while(cicle > 0){
-		            push<<<1, N>>>(dev_Gf, dev_Gf, N, dev_e, dev_h, N);	
+		            pushrelable<<<1, N>>>(dev_Gf, dev_Gf, N, dev_e, dev_h, N);	
                     cudaDeviceSynchronize();
                     
                     cudaMemcpy(host_Gf, dev_Gf, N * N * sizeof(int), cudaMemcpyDeviceToHost);
@@ -208,16 +165,18 @@ namespace parallel {
                     }
                     std::cout << "\n";
 
-                    // for(int i = 0; i < N; i ++){
-                    //     for(int j = 0; j < N; j++){
-                    //         printf("%d ", host_Gf[i*N+j]);
-                    //     }
-                    //     printf("\n");
-                    // }
+                    std::cout << "graph:\n";
+                    for(int i = 0; i < N; i ++){
+                        for(int j = 0; j < N; j++){
+                            printf("%d ", host_Gf[i*N+j]);
+                        }
+                        printf("\n");
+                    }
 
                     // std::cout << "\n";
                     // std::cout << "ExcessTotal: " << excessTotal << std::endl;
-                    // std::cout << ">>>" << "\ncicle: " << cicle << "\ne(0): " << host_e[source] << "\ne[to]: " << host_e[to] << "\nexcessTotal: " << excessTotal; std::cin.ignore();
+                    std::cout << ">>>" << "\ncicle: " << cicle << "\ne(0): " << host_e[source] << "\ne[to]: " << host_e[to] << "\nexcessTotal: " << excessTotal; 
+                    std::cin.ignore();
 
 
                     cicle--;
