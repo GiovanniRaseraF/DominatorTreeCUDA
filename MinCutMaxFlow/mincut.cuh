415f832d30d247fc67d6c87e89cf1cac9e516a9c
Giovanni Rasera <giovanni.raserafarra@gmail.com>
2024-10-29 13:40:56 +0100

    automatic: data exchange

--- MinCutMaxFlow/mincut.cuh
+++ MinCutMaxFlow/mincut.cuh
@@ -164,6 +164,16 @@ namespace sequential {
             }
             printf("\n");
         }
+
+        for(int i = 0; i < num_nodes; i ++){
+            for(int j = 0; j < num_nodes; j++){
+                //printf("%d-%d/%d  ", offsets[i*num_nodes+j], forward_flows[i*num_nodes+j], backward_flows[i*num_nodes+j]);
+                if(offsets[i*num_nodes+j] > 0 && offsets[i*num_nodes+j] == backward_flows[i*num_nodes+j]){
+                    printf("Delete edge: %d -> %d\n", i, j);
+                }
+            }
+            //printf("\n");
+        }
     }
 };
 
