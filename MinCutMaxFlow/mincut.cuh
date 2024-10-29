e40e5d3558710b249eff8dddc5903f3230e128ed
Giovanni Rasera <giovanni.raserafarra@gmail.com>
2024-10-29 13:55:36 +0100

    automatic: data exchange

--- MinCutMaxFlow/mincut.cuh
+++ MinCutMaxFlow/mincut.cuh
@@ -73,7 +73,7 @@ namespace sequential {
             if(offsets[i] > 0){
                 //std::cout << "node before push" << std::endl;
                 int w = i - (v*num_nodes);
-                if (heights[v] == heights[w] + 1) {
+                if (true || heights[v] == heights[w] + 1) {
                     // Push flow
                     int flow = std::min(excesses[v], forward_flows[i]);
                     if (flow == 0) continue;
