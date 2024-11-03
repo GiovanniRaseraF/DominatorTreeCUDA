d26c0927e337ddd3ab50d05e900073c682c17711
Giovanni Rasera <giovanni.raserafarra@gmail.com>
2024-11-03 16:43:14 +0100

    automatic: data exchange

--- MinCutMaxFlow/tests.cuh
+++ MinCutMaxFlow/tests.cuh
@@ -9,5 +9,5 @@ constexpr int V = 7;
 void test1(){
     std::cout << "Test 1" << std::endl;
     Graph G(V, std::vector<int>(V, 0));
-    parallel::GoldbergTarjan::minCutMaxFlow(G, 0, V-1);
+    parallel::GoldbergTarjan::minCutMaxFlow(G, 0, 3);
 }
