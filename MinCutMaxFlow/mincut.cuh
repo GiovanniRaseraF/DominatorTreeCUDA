d0c0d9e728225738f3a3b1c0dd17058840300177
Giovanni Rasera <giovanni.raserafarra@gmail.com>
2024-11-03 16:09:09 +0100

    automatic: data exchange

--- MinCutMaxFlow/mincut.cuh
+++ MinCutMaxFlow/mincut.cuh
@@ -85,14 +85,19 @@ namespace parallel {
         void preflow(
             GPUOffsets offsets,
             GPUrOffsets Roffsets,
+
             GPUDestinations destinations,
             GPUrDestinations Rdestinations,
+
             GPUCapacities capacities,
             GPUrCapacities Rcapacities,
+
             GPUFlowIndex flowIndex,
             GPUHeights heights,
+
             GPUForwardFlow forwardFlows,
             GPUBackwardFlow backwardFlows,
+
             GPUExcesses excesses,
             GPUExcessTotal excessTotal,
             int numNodes,
@@ -115,10 +120,96 @@ namespace parallel {
             } 
         }
 
-        void test(int *o, int l){
-            for(int i = 0; i < l; i++){
-                std::cout << o[i] << std::endl;
+        void print(
+            GPUOffsets offsets,
+            GPUrOffsets Roffsets,
+
+            GPUDestinations destinations,
+            GPUrDestinations Rdestinations,
+
+            GPUCapacities capacities,
+            GPUrCapacities Rcapacities,
+
+            GPUFlowIndex flowIndex,
+            GPUHeights heights,
+
+            GPUForwardFlow forwardFlows,
+            GPUBackwardFlow backwardFlows,
+
+            GPUExcesses excesses,
+            GPUExcessTotal excessTotal,
+            int numNodes,
+            int numEdges,
+            int source,
+            int to
+        ){
+            printf("int offsets[numNodes+1]{");
+            for (int i=0; i < numNodes + 1; i++) {
+                printf("%d, ", offsets[i]);
             }
+            printf("};\n");
+
+            printf("int rOffsets[numNodes+1]{");
+            for (int i=0; i < numNodes + 1; i++) {
+                printf("%d, ", Roffsets[i]);
+            }
+            printf("};\n");
+
+            printf("int destinations[numEdges]{");
+            for (int i=0; i < numEdges; i++) {
+                printf("%d, ", destinations[i]);
+            }
+            printf("};\n");
+
+            printf("int rDestinations[numEdges]{");
+            for (int i=0; i < numEdges; i++) {
+                printf("%d, ", Rdestinations[i]);
+            }
+            printf("};\n");
+
+            printf("int capacities[numEdges]{");
+            for (int i=0; i < numEdges; i++) {
+                printf("%d, ", capacities[i]);
+            }
+            printf("};\n");
+
+            printf("int rCapacities[numEdges]{");
+            for (int i=0; i < numEdges; i++) {
+                printf("%d, ", capacities[i]);
+            }
+            printf("};\n");
+
+            printf("int flowIndex[numEdges]{");
+            for (int i=0; i < numEdges; i++) {
+                printf("%d, ", flowIndex[i]);
+            }
+            printf("};\n");
+
+            printf("int heights[numNodes]{");
+            for (int i=0; i < numNodes; i++) {
+                printf("%d, ", heights[i]);
+            }
+            printf("};\n");
+
+            printf("int forwardFlow[numEdges]{");
+            for (int i=0; i < numEdges; i++) {
+                printf("%d, ", forwardFlows[i]);
+            }
+            printf("};\n");
+
+            printf("int backwardFlows[numEdges]{");
+            for (int i=0; i < numEdges; i++) {
+                printf("%d, ", backwardFlows[i]);
+            }
+            printf("};\n");
+
+            printf("int excesses[numNodes]{");
+            for (int i=0; i < numNodes; i++) {
+                printf("%d, ", excesses[i]);
+            }
+            printf("};\n");
+
+            printf("int excessTotal[1]{%d};\n", Excess_total);
         }
 
         void minCutMaxFlow(Graph &G, int source, int to){
@@ -126,15 +217,46 @@ namespace parallel {
             constexpr int numNodes = 7;
             constexpr int numEdges = 15;
             int offsets[numNodes+1]{0, 6, 8, 11, 13, 15, 18, 18, };
-            int destinations[numEdges]{1, 2, 3, 5, 5, 5, 2, 6, 1, 3, 6, 2, 6, 3, 6, };
-            int capacities[numEdges]{1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, };
-            int forwardFlow[numEdges]{1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 1, 1, 1, };
-            int backwardFlows[numEdges]{0, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 0, 0, 0, };
             int rOffsets[numNodes+1]{0, 0, 2, 5, 8, 10, 13, 18, };
+
+            int destinations[numEdges]{1, 2, 3, 5, 5, 5, 2, 6, 1, 3, 6, 2, 6, 3, 6, };
             int rDestinations[numEdges]{0, 2, 0, 1, 3, 0, 2, 4, 5, 5, 0, 0, 0, 1, 2, };
+
+            int capacities[numEdges]{1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, };
+            int rCapacities[numEdges]{1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, };
+
             int flowIndex[numEdges]{0, 8, 1, 6, 11, 2, 9, 13, 0, 0, 3, 3, 3, 7, 10, };
-            int heights[numNodes]{7, 6, 6, 0, 0, 2, 7, };
-            int excesses[numNodes]{0, 0, 0, 2, 0, 0, 1, };
+            int heights[numNodes]{0, 0, 0, 0, 0, 0, 0, };
+
+            int forwardFlow[numEdges]{1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, };
+            int backwardFlows[numEdges]{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, };
+            int excesses[numNodes]{0, 0, 0, 0, 0, 0, 0, };
+
+            int excessTotal[1]{352493152};
+
+            print(
+                offsets,
+                rOffsets,
+
+                destinations,
+                rDestinations,
+
+                capacities,
+                rCapacities,
+
+                flowIndex,
+                heights,
+
+                forwardFlow,
+                backwardFlows,
+                excesses,
+
+                excessTotal,
+                numNodes,
+                numEdges,
+                source,
+                to
+            );
         }
     };
 };
