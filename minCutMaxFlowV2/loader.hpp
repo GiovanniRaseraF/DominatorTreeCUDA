c7d1f8b26f2ec8045060f668dab088c6c1c8d3e2
Giovanni Rasera <giovanni.raserafarra@gmail.com>
2024-11-11 15:00:49 +0100

    automatic: data exchange

--- minCutMaxFlowV2/loader.hpp
+++ minCutMaxFlowV2/loader.hpp
@@ -5,11 +5,13 @@
 #include <unordered_map>
 #include <fstream>
 #include <iomanip>
+#include <algorithm>
 #include <sstream>
 
-namespace loader {
+namespace loader
+{
     void buildFromTxtFile(
-        const std::string &filename, 
+        const std::string &filename,
         int &num_nodes,
         int &num_edges,
         int &num_edges_processed,
@@ -17,48 +19,145 @@ namespace loader {
         int &sink_node,
         std::vector<int> &destinations,
         std::vector<int> &offsets,
-        std::vector<int> &capacities) {
+        std::vector<int> &capacities){
         std::ifstream file(filename);
-    if (file.fail()) {
-        fprintf(stderr, "\"%s\" does not exist!\n", filename.c_str());
-        exit(1);
-    }
-    std::string line;
-    std::unordered_map<int, std::vector<int>> adjacency_list;
-    std::unordered_map<int, std::vector<int>> cap_list;
-    int cnt = 0;
-    while (std::getline(file, line)) {
-        std::stringstream ss(line);
-        if (line.find("# Nodes:") != std::string::npos)
-        sscanf(line.c_str(), "# Nodes: %d Edges: %d", &num_nodes, &num_edges);
-
-        if (ss.str()[0] == '#')
-        continue;
-        int from, to, cap;
-        ss >> from >> to >> cap;
-        adjacency_list[from].push_back(to);
-        cap_list[from].push_back(cap);
-        cnt++;
-    }
-
-    // num_nodes = adjacency_list.size();
-    offsets.push_back(0);
-    for (int i = 0; i < num_nodes; ++i) {
-        // some nodes have no out edges
-        if (adjacency_list.count(i)==0) {
-        offsets.push_back(offsets.back());
-        continue;
+        if (file.fail()){
+            fprintf(stderr, "\"%s\" does not exist!\n", filename.c_str());
+            exit(1);
         }
-        sort(adjacency_list[i].begin(), adjacency_list[i].end());
-        for (int neighbor : adjacency_list[i]) {
-        destinations.push_back(neighbor);
+        std::string line;
+        std::unordered_map<int, std::vector<int>> adjacency_list;
+        std::unordered_map<int, std::vector<int>> cap_list;
+        int cnt = 0;
+        while (std::getline(file, line)){
+            std::stringstream ss(line);
+            if (line.find("# Nodes:") != std::string::npos)
+                sscanf(line.c_str(), "# Nodes: %d Edges: %d", &num_nodes, &num_edges);
+
+            if (ss.str()[0] == '#')
+                continue;
+            int from, to, cap;
+            ss >> from >> to >> cap;
+            adjacency_list[from].push_back(to);
+            cap_list[from].push_back(cap);
+            cnt++;
         }
-        for (int cap: cap_list[i]) {
-        capacities.push_back(cap);
+
+        // num_nodes = adjacency_list.size();
+        offsets.push_back(0);
+        for (int i = 0; i < num_nodes; ++i){
+            // some nodes have no out edges
+            if (adjacency_list.count(i) == 0){
+                offsets.push_back(offsets.back());
+                continue;
+            }
+            sort(adjacency_list[i].begin(), adjacency_list[i].end());
+            for (int neighbor : adjacency_list[i]){
+                destinations.push_back(neighbor);
+            }
+            for (int cap : cap_list[i]){
+                capacities.push_back(cap);
+            }
+            offsets.push_back(destinations.size());
         }
-        offsets.push_back(destinations.size());
+
+        num_edges_processed = cnt;
     }
 
-    num_edges_processed = cnt;
-    }   
+    void buildFromCSRGraph(
+        int &num_nodes,
+        int &num_edges,
+        int &num_edges_processed,
+        int &source_node,
+        int &sink_node,
+        std::vector<int> &graph_destinations,
+        std::vector<int> &graph_offsets,
+        std::vector<int> &graph_capacities,
+        // to load
+        int *offsets,
+        int *destinations,
+        int *capacities,
+        int *forward_flows,
+        int *roffsets,
+        int *rdestinations,
+        int *backward_flows,
+        int *flow_index,
+        int *heights,
+        int *excesses){
+        /* Allocate offsets, destinations, capacities, flows, roffsets, rdestinations, rflows */
+        offsets = (int *)malloc(sizeof(int) * (num_nodes + 1));
+        destinations = (int *)malloc(sizeof(int) * num_edges);
+        capacities = (int *)malloc(sizeof(int) * num_edges);
+        forward_flows = (int *)malloc(sizeof(int) * num_edges);
+        roffsets = (int *)malloc(sizeof(int) * (num_nodes + 1));
+        rdestinations = (int *)malloc(sizeof(int) * num_edges);
+        backward_flows = (int *)malloc(sizeof(int) * num_edges);
+        flow_index = (int *)malloc(sizeof(int) * num_edges);
+        heights = (int *)malloc(sizeof(int) * num_nodes);
+        excesses = (int *)malloc(sizeof(int) * num_nodes);
+
+        for (int i = 0; i < num_nodes; i++){
+            heights[i] = 0;
+            excesses[i] = 0;
+        }
+
+        // Initialize offset vectors
+        for (int i = 0; i < num_nodes + 1; ++i){
+            offsets[i] = graph_offsets[i];
+            roffsets[i] = 0;
+        }
+        for (int i = 0; i < num_edges; ++i){
+            destinations[i] = graph_destinations[i];
+            capacities[i] = graph_capacities[i];
+            forward_flows[i] = graph_capacities[i]; // The initial residual flow is the same as capacity, not 0
+            backward_flows[i] = 0;
+        }
+
+        std::vector<int> backward_counts(num_nodes, 0);
+
+        // Count the number of edges for each node to prepare the offset vectors
+        for (int i = 0; i < num_nodes; ++i){
+            for (int j = graph_offsets[i]; j < graph_offsets[i + 1]; ++j){
+                backward_counts[graph_destinations[j]]++;
+            }
+        }
+
+        // Convert counts to actual offsets
+        for (int i = 1; i <= num_nodes; ++i){
+            roffsets[i] = roffsets[i - 1] + backward_counts[i - 1];
+        }
+
+        // Initialize backward count vector
+        backward_counts.clear();
+        for (int i = 0; i < num_nodes; ++i){
+            backward_counts[i] = 0;
+        }
+
+        // Fill forward and backward edges
+        for (int i = 0; i < num_nodes; ++i){
+            for (int j = graph_offsets[i]; j < graph_offsets[i + 1]; ++j){
+                int dest = graph_destinations[j];
+
+                // Corresponding backward edge
+                int backward_index = roffsets[dest] + backward_counts[dest];
+                rdestinations[backward_index] = i;
+                backward_counts[dest]++;
+            }
+        }
+
+        // Initialize flow index
+        for (int u = 0; u < num_nodes; u++){
+            for (int i = roffsets[u]; i < roffsets[u + 1]; i++){
+                int v = rdestinations[i];
+
+                // Find the forward edge index
+                for (int j = offsets[v]; j < offsets[v + 1]; j++){
+                    if (destinations[j] == u){
+                        flow_index[i] = j;
+                        break;
+                    }
+                }
+            }
+        }
+    }
 };
