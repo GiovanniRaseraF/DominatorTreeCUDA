3e24f8b61f818f3e05a34d73c4b05bf140a6659d
Giovanni Rasera <giovanni.raserafarra@gmail.com>
2024-11-14 15:51:20 +0100

    automatic: data exchange

--- minCutMaxFlowV2/mincut.hpp
+++ minCutMaxFlowV2/mincut.hpp
@@ -152,7 +152,7 @@ namespace parallel {
             (cudaMemcpy(gpu_flow_index,     flow_index,     numEdges*sizeof(int), cudaMemcpyHostToDevice));
 
             // algo start
-            while((excess_flow[source] + excess_flow[sink]) < *excessTotal){
+            while((excess_flow[source+1] + excess_flow[sink]) < *excessTotal){
                 (cudaMemcpy(gpu_height,        heights,         V*sizeof(int), cudaMemcpyHostToDevice));
                 (cudaMemcpy(gpu_excess_flow,   excess_flow,     V*sizeof(int), cudaMemcpyHostToDevice));
                 (cudaMemcpy(gpu_fflows,        fflow,           E*sizeof(int), cudaMemcpyHostToDevice));
