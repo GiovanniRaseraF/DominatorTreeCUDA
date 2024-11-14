6e4c6d69442c86f3d526b6593fd8d9f724a236f7
Giovanni Rasera <giovanni.raserafarra@gmail.com>
2024-11-14 17:26:31 +0100

    automatic: data exchange

---
+++ minCutMaxFlowV2/graphs/clean.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+#include <sstream>
+
+int main(){
+    std::string first = "";
+    std::getline(std::cin, first);
+    std::cout << first << std::endl;
+
+    std::string line;
+
+    while (std::getline(std::cin, line)) {
+        std::stringstream ss{line};
+
+        int u, v, c;
+        ss >> u >> v >> c;
+        std::cout << u  << " " << v  << " " << 1 << std::endl;
+    }
+}
