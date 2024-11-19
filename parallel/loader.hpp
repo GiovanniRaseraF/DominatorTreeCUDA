c0d8d4ad7c6e6912b869afa689f7c47ccec90a2c
Giovanni Rasera <giovanni.raserafarra@gmail.com>
2024-11-19 17:41:43 +0100

    automatic: data exchange

--- parallel/loader.hpp
+++ parallel/loader.hpp
@@ -88,6 +88,71 @@ namespace loader{
         num_edges_processed = cnt;
     }
 
+    // Build the Graph for Node Cut
+    void buildFromTxtFileForNodeCut(
+        const std::string &filename,
+        int &num_nodes,
+        int &num_edges,
+        int &num_edges_processed,
+        int &source_node,
+        int &sink_node,
+        std::vector<int> &destinations,
+        std::vector<int> &offsets,
+        std::vector<int> &capacities){
+        std::ifstream file(filename);
+        if (file.fail()){
+            fprintf(stderr, "\"%s\" does not exist!\n", filename.c_str());
+            exit(1);
+        }
+        std::string line;
+        std::unordered_map<int, std::vector<int>> adjacency_list;
+        std::unordered_map<int, std::vector<int>> cap_list;
+
+        int cnt = 0;
+        while (std::getline(file, line)){
+            std::stringstream ss(line);
+            if (line.find("# Nodes:") != std::string::npos){
+                sscanf(line.c_str(), "# Nodes: %d Edges: %d", &num_nodes, &num_edges);
+                num_edges += num_nodes;
+                num_nodes *= 2;
+            }
+
+            if (ss.str()[0] == '#')
+                continue;
+            int from, to, cap;
+            ss >> from >> to >> cap;
+            // basic one 
+            adjacency_list[from*2+1].push_back(to*2);
+            cap_list[from*2+1].push_back(num_nodes*2);
+            cnt++;
+        }
+
+        for(int i = 0; i < num_nodes/2; i++){
+            adjacency_list[i*2].push_back(i*2+1);
+            cap_list[i*2].push_back(1);
+        }
+
+        // num_nodes = adjacency_list.size();
+        offsets.push_back(0);
+        for (int i = 0; i < num_nodes; ++i){
+            // some nodes have no out edges
+            if (adjacency_list.count(i) == 0){
+                offsets.push_back(offsets.back());
+                continue;
+            }
+            sort(adjacency_list[i].begin(), adjacency_list[i].end());
+            for (int neighbor : adjacency_list[i]){
+                destinations.push_back(neighbor);
+            }
+            for (int cap : cap_list[i]){
+                capacities.push_back(cap);
+            }
+            offsets.push_back(destinations.size());
+        }
+
+        num_edges_processed = cnt;
+    }
+
     /*  This function create the CSR representation of the graph
         this is useful when using the graph in the GPU
     */
